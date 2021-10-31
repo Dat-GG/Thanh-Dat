6162d8cfc57ca34fe2259a42ce5a4a26fe3c83fa
Dat-GG <lethanhdat160796@gmail.com>
2021-10-31 19:53:42 +0700

    Square_Roll

---
+++ Assets/Resoucers/Scripts/SquareController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareController : MonoBehaviour
+{
+    public int i;
+    public int a;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tranpost();
+    }
+
+    void Tranpost()
+    {
+        var a = i + 1; 
+
+        if (this.gameObject.transform.position.x == 0 && this.gameObject.transform.position.y == 0)
+        {
+            this.gameObject.transform.position += new Vector3(i, 0, 0);
+            Debug.LogError(this.gameObject.transform.position.x  + " gia tri cua x ");
+            Debug.LogError(this.gameObject.transform.position.y  + " gia tri cua y ");
+        }
+
+        else if (this.gameObject.transform.position.x >= i && this.gameObject.transform.position.y == 0)
+        {
+            this.gameObject.transform.position += new Vector3(0, i, 0);
+            Debug.LogError(this.gameObject.transform.position.x + " gia tri cua x ");
+            Debug.LogError(this.gameObject.transform.position.y + " gia tri cua y ");
+        }
+        else if (this.gameObject.transform.position.x >= i && this.gameObject.transform.position.y >= i)
+        {
+            this.gameObject.transform.position += new Vector3(-i, 0, 0);
+            Debug.LogError(this.gameObject.transform.position.x + " gia tri cua x ");
+            Debug.LogError(this.gameObject.transform.position.y + " gia tri cua y ");
+        }
+        else if (this.gameObject.transform.position.x == 0 && this.gameObject.transform.position.y >= 0)
+        {
+            this.gameObject.transform.position += new Vector3(0, -i, 0);
+            Debug.LogError(this.gameObject.transform.position.x + " gia tri cua x ");
+            Debug.LogError(this.gameObject.transform.position.y + " gia tri cua y ");
+        }
+                    }
+                }
+
+            
+        
+
+    
